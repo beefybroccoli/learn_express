c004f05774a3f2dd95cd52b5b8e50485310632a5
beefybroccoli <tedkimemail@yahoo.com>
2021-08-12 16:33:43 -0700

    (no comment)

--- express-app/index.js
+++ express-app/index.js
@@ -4,8 +4,33 @@ import MOCK_DATA from "./MOCK_DATA.json";
 const app = express();
 const PORT = 3000;
 
-const path_get = "/";
-app.get(path_get, (request, response) => {
+const path_post = "/post";
+app.post(path_post, (request, response) => {
+  response.send(`a get request with ${path_post} route on port ${PORT}`);
+});
+
+const path_put = "/put";
+app.put(path_put, (request, response) => {
+  response.send(`a put request with ${path_put} route on port ${PORT}`);
+});
+
+const path_delete = "/delete";
+app.delete(path_delete, (request, response) => {
+  response.send(`a delete request with ${path_delete} route on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Your sever is running on port ${PORT}`);
+  // console.log(MOCK_DATA[0]);
+  console.log("MOCK_DATA.length = ", MOCK_DATA.length);
+  console.log("========================================");
+});
+
+/**
+ * @returns the whole MOCK_DATA file
+ */
+const path_root = "/";
+app.get(path_root, (request, response) => {
   //   response.send(`a get request with ${path} route on port ${PORT}`);
 
   //get data from source (ie. database, API call)
@@ -14,38 +39,84 @@ app.get(path_get, (request, response) => {
   response.json(MOCK_DATA);
 });
 
-const path_post = "/newItem";
-app.post(path_post, (request, response) => {
-  response.send(`a get request with ${path_post} route on port ${PORT}`);
-});
-
-const path_put = "/item";
-app.put(path_put, (request, response) => {
-  response.send(`a put request with ${path_put} route on port ${PORT}`);
-});
-
-const path_delete = "/item";
-app.delete(path_delete, (request, response) => {
-  response.send(`a delete request with ${path_delete} route on port ${PORT}`);
-});
-
-app.listen(PORT, () => {
-  console.log(`Your sever is running on port ${PORT}`);
-  console.log(MOCK_DATA[0]);
-  console.log("MOCK_DATA.length = ", MOCK_DATA.length);
-  console.log("========================================");
-});
-
 /*********
  * serving resources via public  path
  */
-const folder_public = "public";
-app.use(express.static(folder_public));
+let folder_name = "public";
+app.use(express.static(folder_name));
 //load with this url, http://localhost:3000/tent.jpg
 
 /**
  * serving resources via other path
  */
-const path_image = "/images";
+// this is for images folder on path images
+folder_name = "images";
+const path_image = path_root + folder_name;
 app.use(path_image, express.static("images"));
 //load with this url, http://localhost:3000/images/rocket.jpg
+
+/**
+ * serve resources with a parameter, like Netflix.com
+ */
+// const path_root = "/"; define above
+const path_get = "/get/:";
+const parameter_id = "id";
+//"/get/:id"
+app.get(path_get + parameter_id, (request, response) => {
+  //   console.log(request.params.id); //return 04
+  console.log("execute line 62");
+  let id = Number(request.params.id);
+  console.log(id); //return 4
+  console.log(MOCK_DATA[id - 1]);
+
+  response.send(MOCK_DATA[id - 1]);
+});
+// call http://localhost:3000/get/04
+// return below
+//   4
+//   {
+//     id: 4,
+//     first_name: 'Madeline',
+//     last_name: 'Lamprecht',
+//     email: 'mlamprecht3@jalbum.net',
+//     gender: 'Genderfluid'
+//   }
+
+const path_two_parameters = "/get/:category/:id";
+app.get(path_two_parameters, (request, response) => {
+  console.log(request.params.category);
+  console.log(request.params.id);
+  let id = Number(request.params.id);
+  console.log(id); //return 4
+  console.log(MOCK_DATA[id - 1]);
+  // http://localhost:3000/get/old/100
+  // http://localhost:3000/get/young/99
+  response.send(MOCK_DATA[id - 1]);
+});
+
+const cb_error_handling = (request, response) => {
+  console.log("Did you get the right data?");
+};
+
+const cb_response_id = (request, response, next) => {
+  console.log("execute line 98");
+  //   console.log(request.params.id); //return 04
+  let id = Number(request.params.id);
+  //   console.log(id); //return 4
+  console.log(MOCK_DATA[id - 1]);
+
+  response.send(MOCK_DATA[id - 1]);
+  return next(); //next, jump to the next function
+};
+
+/**
+ * only one response per call
+ */
+const path_parameter_ = "/get/:id";
+//"gender":"Male"
+//"first_name":"Brady"
+//"last_name":"Naris"
+//"id":98,
+//"email":"bcorbally2p@ustream.tv"
+console.log("============================================");
+app.get(path_parameter_, cb_response_id, cb_error_handling);
